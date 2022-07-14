59b62ba99dd2d66070bedd6e75c616f1f7fbc81f
Carlos Rodriguez <crrodriguez.0430@gmail.com>
2022-07-14 15:22:37 -0700

    remaining stubs

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('/places', require('.controllers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('greetings humans')
+    res.send('Hello world!')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>wrong way homie</h1>')
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 app.listen(process.env.PORT)
